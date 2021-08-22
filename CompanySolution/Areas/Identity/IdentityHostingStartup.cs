2fa15527c0c4d8f1cc8f5ae48c0db3c38d1ff1ac
George <georgiosloulakis@gmail.com>
2021-08-22 11:31:38 +0300

    Identity

---
+++ CompanySolution/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using CompanySolution.Data;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.UI;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+[assembly: HostingStartup(typeof(CompanySolution.Areas.Identity.IdentityHostingStartup))]
+namespace CompanySolution.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => {
+            });
+        }
+    }
+}
